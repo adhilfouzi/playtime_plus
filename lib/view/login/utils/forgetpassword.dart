01028d4c7176c7f7ae7c0405aa16669d18d90f2e
Adhil Fouzi <adhilfouzi@gmail.com>
2024-06-10 17:55:54 +0530

    reset forget password method

--- lib/view/login/utils/forgetpassword.dart
+++ lib/view/login/utils/forgetpassword.dart
@@ -17,15 +17,15 @@ class ForgetPasswordLink extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () async {
-        if (emailController.text.isNotEmpty) {
-          await AuthenticationRepository()
-              .sendPasswordResetEmail(emailController.text.toLowerCase());
-          log('sendPasswordResetEmail');
-        } else if (emailController.text.isEmpty) {
+        if (emailController.text.isEmpty) {
           CustomSnackBar.showInfo(context, "Email cannot be empty");
         } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
             .hasMatch(emailController.text)) {
           CustomSnackBar.showInfo(context, 'Invalid email format');
+        } else if (emailController.text.isNotEmpty) {
+          await AuthenticationRepository()
+              .sendPasswordResetEmail(emailController.text.toLowerCase());
+          log('sendPasswordResetEmail');
         }
       },
       child: Text(
